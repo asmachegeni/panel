d4fa879cea0e7fd997e989df467434777e6ee6c8
Asma <as.ch.2280@gmail.com>
2024-08-14 19:48:57 +0330

    add e2e test example

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,20 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("has title", async ({ page }) => {
+  await page.goto("http://localhost:5173/graph");
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle("نمایش گراف");
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test("check tab and back to home", async ({ page }) => {
+  await page.goto("http://localhost:5173/graph");
+  await page.getByLabel("انتخاب ارتباط").click();
+  await page.getByRole("tab", { name: "ویرایش گراف" }).click();
+  await page.getByRole("tab", { name: "Rotary" }).click();
+  await page.getByRole("tab", { name: "ویرایش گراف" }).click();
+  await page.getByRole("tab", { name: "گراف کلی" }).click();
+  await page.getByRole("button", { name: "برگشت به خانه" }).click();
+  await expect(page).toHaveTitle(" فایل ها ");
 });
+
